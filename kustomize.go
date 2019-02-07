3f631c640fb9337f2ee47ac5d57d6b585e2a71fb
Travis Alpers <arbiter34@gmail.com>
2019-02-07 10:56:46 -0700

    Fixed import order.

--- kustomize.go
+++ kustomize.go
@@ -19,10 +19,9 @@ package main
 import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"os"
-	"sigs.k8s.io/kustomize/pkg/types"
-
 	"sigs.k8s.io/kustomize/k8sdeps"
 	"sigs.k8s.io/kustomize/pkg/commands"
+	"sigs.k8s.io/kustomize/pkg/types"
 )
 
 func main() {
