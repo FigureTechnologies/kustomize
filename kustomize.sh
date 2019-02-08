b5fbb29dab8bc0668152c47efefc7850017d9ecb
Travis Alpers <arbiter34@gmail.com>
2019-02-08 09:25:51 -0700

    Added explicit pathing for those that don't have /Users/talpers/go/bin on their PATH

--- kustomize.sh
+++ kustomize.sh
@@ -5,8 +5,8 @@ TEMP_GO111MODULE=$GO111MODULE
 export GO111MODULE=off
 cd ~
 go get golang.org/dl/go1.12beta1
-go1.12beta1 download
+$GOPATH/bin/go1.12beta1 download
 export GO111MODULE=on
-go1.12beta1 get github.com/FigureTechnologies/kustomize@v2.0.0-figure
+$GOPATH/bin/go1.12beta1 get github.com/FigureTechnologies/kustomize@v2.0.0-figure
 export GO111MODULE=$TEMP_GO111MODULE
 cd $TEMP_CWD
